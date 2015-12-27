067fc81cd639e22b9d5bb1a1efe4b0c2ece6a29d
rina basu <ribasu@tegna.com>
2015-12-26 21:06:04 -0500

    changes to code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of functions create a square invertible matrix 
+## (makeCacheMatrix function) and make the inverse of the matrix 
+## available in the cache environment (cacheSolve function)
+##
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL 
+     	set <- function(y) { 
+        x <<- y 
+        inv <<- NULL 
+     } 
+    get <- function() x 
+     setinverse <- function(inverse) inv <<- inverse 
+    getinverse <- function() inv 
+     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
+ 
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+inv <- x$getinverse() 
+   if(!is.null(inv)) { 
+        message("getting cached data.") 
+         return(inv) 
+    } 
+    data <- x$get() 
+    inv <- solve(data) 
+     x$setinverse(inv) 
+    inv 
+ } 
+
+
+## test
+## 
+## > source("cachematrix.R")    load cachematrix.R program
+## > a <- makeCacheMatrix()     create cachematrix function
+## > a$set(matrix(1:4, 2, 2))   create matrix in working environment
+## > cacheSolve(a)              first time you run it returns inverted matrix
+##                              
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+##
+## > cacheSolve(a)              the second time you run it returns
+##                              inverted matrix from cache
+## getting cached data          
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
